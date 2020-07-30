7c61c207639fba6b904cc51dfce7ae6f35e3c270
Rodrigo Revilla <rorevillaca@gmail.com>
2020-07-30 14:24:59 -0500

    Completed vis script

--- monopoly_simulator.py
+++ monopoly_simulator.py
@@ -1,6 +1,8 @@
 import random
 import string
 import csv
+from math import floor
+import collections
 
 class Player(object):
     def __init__(self):
@@ -35,7 +37,7 @@ class Player(object):
         die_two = random.randint(1, 6) #Rolls die
         roll_total = die_one + die_two #Adds up both dice
         last_position = self.landed_spaces[-1] #Recovers position before rolling dice
-        self.current_position = (last_position + roll_total)%40 #Calculates position after rolling dice, considering the board "loop"
+        self.current_position = floor((last_position + roll_total)%40) #Calculates position after rolling dice, considering the board "loop"
 
         #Checks wether doubles were rolled
         if die_one == die_two: 
@@ -70,7 +72,7 @@ class Player(object):
                 elif drawn_card == "H":
                     new_position = self.current_position -3
                 elif drawn_card == "I":
-                    new_position = 10
+                    new_position = 10.5
                 elif drawn_card == "L":
                     new_position = 5
                 elif drawn_card == "M":
@@ -83,12 +85,12 @@ class Player(object):
                 if drawn_card == "a":
                     new_position = 0
                 elif drawn_card == "f":
-                    new_position = 10
+                    new_position = 10.5
                 else:
                     special_condition = False
                 #print("drew chance card:"+drawn_card)
             elif self.current_position == 30:
-                new_position = 10
+                new_position = 10.5
 
             
         #Checks for three doubles in a row or landing in a "special condition" space
@@ -101,7 +103,7 @@ class Player(object):
                 
             else:
                 #Takes player to jail
-                self.landed_spaces.append(10)
+                self.landed_spaces.append(10.5)
 
         else: #Updates position if player not taken to jail
             self.landed_spaces.append(self.current_position)
@@ -119,7 +121,7 @@ game_index = []
 total_landed_spaces = []
 
 #Generate one million games
-for i in range(1,1000001):
+for i in range(1,51):
     rodrigo = Player()
     game=False
     while not game:
@@ -128,10 +130,13 @@ for i in range(1,1000001):
     game_index += [i]*len(rodrigo.landed_spaces)
     total_landed_spaces += rodrigo.landed_spaces
 
+counter=collections.Counter(total_landed_spaces)
+print(counter)
+
 #Save to csv file
-with open(r'C:\Users\rodrigo.revilla\Documents\simulation_results.csv', 'w',newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(zip(game_index, total_landed_spaces))
+#with open(r'C:\Users\rodrigo.revilla\Documents\simulation_results.csv', 'w',newline='') as f:
+#    writer = csv.writer(f)
+#    writer.writerows(zip(game_index, total_landed_spaces))
 #print(game_index)
 #print(total_landed_spaces)
 
--- monopoly_viz.R
+++ monopoly_viz.R
@@ -2,16 +2,25 @@
 
 library(tidyverse)
 library(hrbrthemes)
+library(openxlsx)
+library(magrittr)
+library(extrafont)
+library(ggnewscale)
+library(cowplot)
+
+font_import(paths = c("C:/Users/rodrigo.revilla/AppData/Local/Microsoft/Windows/Fonts", prompt = F))
+install.packages("extrafontdb")
+loadfonts()
 
 #Data wrangling
 
 
 #Generating dataframe
-ymins <- c(rep(0,11),seq(1,5.5,0.5),rep(5.5,9),seq(5.5,1,-0.5))
-ymaxs <- c(rep(1,10),seq(1,5.5,0.5),6.5,rep(6.5,10),seq(5.5,1.5,-0.5))
-xmins <- c(seq(5.5,1,-0.5),rep(0,11),seq(1,5.5,0.5),rep(5.5,9))
-xmaxs <- c(6.5,seq(5.5,1.5,-0.5),rep(1,11),seq(1.5,5.5,0.5),rep(6.5,10))
-id<-seq(1,40)
+ymins <- c(rep(0,11),0.4,seq(1,5.5,0.5),rep(5.5,9),seq(5.5,1,-0.5))
+ymaxs <- c(rep(1,11),1,seq(1.5,5.5,0.5),6.5,rep(6.5,10),seq(5.5,1.5,-0.5))
+xmins <- c(seq(5.5,1,-0.5),0,0.4,rep(0,10),seq(1,5.5,0.5),rep(5.5,9))
+xmaxs <- c(6.5,seq(5.5,1.5,-0.5),rep(1,12),seq(1.5,5.5,0.5),rep(6.5,10))
+id<-c(seq(0,10),10.5,seq(11,39))
 tiles <- data.frame(xmins,xmaxs,ymins,ymaxs,id)
 
 ymins <- c(rep(0.8,5),1,2,2.5,3.5,4.5,5,rep(5.5,6),5,4.5,3.5,2,1)
@@ -21,18 +30,44 @@ xmaxs <-c(5.5,4.5,3,2,1.5,rep(1,6),1.5,2.5,3,4,4.5,5.5,rep(5.7,5))
 colors <- c(rep("#945438",2),rep("#abe0f8",3),rep("#d93a96",3),rep("#f7941d",3),rep("#ed1b24",3),rep("#fef200",3),rep("#20b15a",3),rep("#0172ba",2))
 group_tiles <- data.frame(xmins,xmaxs,ymins,ymaxs,colors)
 
+simulation_results <- read.csv2('C:/Users/rodrigo.revilla/Desktop/simulation_results.csv',sep=",",header=FALSE)
+simulation_results %<>% group_by(V2) %>% summarise(n=n()) %>% ungroup()
+simulation_results %<>% mutate(percentage=n/sum(simulation_results$n)) 
+simulation_results %<>% mutate(V2 = V2 %>% as.character() %>% as.numeric())
+
+tiles  %<>%  left_join(simulation_results %>% select(-n),by=c("id"="V2"))
+tiles  %<>%  mutate(perc_label = scales::percent(percentage,accuracy = 0.1),x_lab=xmins+(xmaxs-xmins)/2,y_lab=ymins+(ymaxs-ymins)/2)
+tiles  %<>%  mutate(y_lab=ifelse(id%in%c(0,10),0.250,y_lab)) %>% mutate(y_lab=ifelse(id%in%c(10.5),0.8,y_lab))
+
+
 #Plot creation
 
-ggplot()+
-  geom_rect(data=tiles,aes(xmin=xmins,xmax=xmaxs,ymin=ymins,ymax=ymaxs,fill=id),color="black")+
-  scale_fill_gradient()+
+a<-ggplot()+
+  geom_rect(data=tiles,aes(xmin=xmins,xmax=xmaxs,ymin=ymins,ymax=ymaxs,fill=percentage),color="white")+
+  scale_fill_continuous(low = "#fde5d9",high = "#c41e1e",labels = scales::percent_format(accuracy=0.1),name="Probability",guide=guide_legend(keywidth = 1))+
   new_scale_fill()+
-  geom_rect(data=group_tiles,aes(xmin=xmins,xmax=xmaxs,ymin=ymins,ymax=ymaxs,fill=colors),color="black")+
+  geom_rect(data=group_tiles,aes(xmin=xmins,xmax=xmaxs,ymin=ymins,ymax=ymaxs,fill=colors),color="white")+
   scale_fill_identity()+
-  scale_y_continuous(limits=c(-0.5,7))+
-  scale_x_continuous(limits=c(-0.5,7))+
+  geom_text(data=tiles,aes(x=x_lab,y=y_lab,label=perc_label,color=ifelse(id%in%c(0,10.5),"white","black")),size=4,fontface="bold")+
+  scale_color_manual(values=c("black","white"),guide=FALSE)+
+  geom_label(aes(x=6.5/2-0.4,y=(6.5/2)+1.3,label="THE PROBABILITIES OF"),size=5,color="white",fill="gray60",family="Kabel-Heavy",label.padding = unit(0.4,"lines"))+
+  geom_label(aes(x=6.5/2,y=(6.5/2)+0.8,label="MONOPOLY"),size=12,color="white",fill="#ee1b24",family="Kabel-Heavy",label.padding = unit(0.5,"lines"))+
+  geom_text(aes(x=6.5/2,y=2.4,label="SPACE COLOR INDICATES THE RELATIVE \n PROBABILITY OF LANDING OVER 500,000\n SIMULATIONS OF THE GAME\n\nConsiders dice rolls, 'the three-doubles rule',\nCommunity Chest and Chance Cards, and \nlanding on the 'Go To Jail' Tile"),fontface="bold",color="gray50")+
+  geom_text(aes(x=6,y=0.6,label="GO"),color="white",family="Kabel-Heavy",size=5)+
+  geom_text(aes(x=0.7,y=0.62,label="JAIL"),color="white",family="Kabel-Heavy",size=3)+
+  geom_text(aes(x=2.75,y=5.25,label="ILLINOIS AVE."),family="Kabel-Heavy",size=4,color="gray55")+
+  geom_segment(aes(x=2.75,y=5.48,xend=2.75,yend=5.35),size=0.8,color="gray60")+
+  geom_segment(aes(x=6.2,y=0.45,xend=5.8,yend=0.45),color="white",arrow = arrow(length = unit(0.15, "cm")),size=0.6)+
+  scale_y_continuous(limits=c(-0.5,7),expand=c(-.05,-.05))+
+  scale_x_continuous(limits=c(-0.5,7),expand=c(-.05,-.05))+
   coord_fixed()+
-  theme_void()
+  theme_void()+
+  theme(legend.position = "bottom",legend.margin=margin(unit(0,"cm")))
+  
+additional_elements <- ggdraw()
+additional_elements <- additional_elements + draw_label("github.com/Rorevilla", x = 0.89, y = 0.04,colour = "gray50",fontface="bold",size=8.5)
+
+final_plot<-additional_elements+draw_plot(a+ theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = "in")))
 
 new_scale <- function(new_aes) {
   structure(ggplot2::standardise_aes_names(new_aes), class = "new_aes")
